5dfc0ba1dfc8346617f800ef54257d1672119d07
Lalenadr <81616554+Lalenadr@users.noreply.github.com>
2021-05-25 14:49:45 +0200

    Add files via upload

---
+++ getal.js
@@ -0,0 +1,18 @@
+ // random value -> dit werkt
+let y = Math.floor(Math.random() * 10 + 1);
+      
+ // number guessed by user haalt hij denk ik op via id invoer    
+document.getElementById("submit").onclick = function(){
+     
+let x = document.getElementById("invoer").value;
+
+if(x == y)
+{    
+    alert("Gefeliciflanstaart! Het is juist! Je nummer was juist! ");
+}
+else
+{    
+    alert("Nope!! Try again");
+}
+
+}
